c8dd17861c95337cf4d1e44f1365dbdbbc546000
ValleLuca <valle.lucaAb@outlook.it>
2021-12-13 16:56:56 +0100

    Conclusa mail, inizio modifiche estetiche

--- Mail/myscipt.js
+++ Mail/myscipt.js
@@ -1,14 +1,21 @@
 const aggMail = prompt("Inserire la propria mail"); // input utente attraverso l'utente
+let mailPresente = false; //variabile booleana per verificare le mail nell'array
+let stampaMail = document.getElementById("mail");
 
 const mailnelDatabase = [ "pippo@gmail.com", "pluto@gmail.com", "105@gmail.com" ]; //aggiunta array con mail preinserite
 
 for(let i = 0; i < mailnelDatabase.length; i++){ //creare un ciclo per controllare se si ha gia una mail
     
     if( mailnelDatabase[i] == aggMail){
-        console.log("Mail già usata"); //Se la mail è stata già confermata stampare "mail già usata"
-    }
-    else if (mailnelDatabase[i] != aggMail){
-        console.log("Mail disponibile"); //confermare mail se non è stata già utilizzata
+        mailPresente = true; //Se la mail è stata già confermata stampare "mail già usata"
     }
 } 
 
+if( mailPresente ){
+    console.log("Mail già usata"); //Se la mail è stata già confermata stampare "mail già usata"
+    stampaMail.innerHTML = "Mail già usata";
+}
+else {
+    console.log("Mail disponibile"); //confermare mail se non è stata già utilizzata
+    stampaMail.innerHTML = "Mail disponibile";
+}
