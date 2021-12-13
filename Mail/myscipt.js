34d39a98d2fe3f73a310363fc8c92b9c5135ffb5
ValleLuca <valle.lucaAb@outlook.it>
2021-12-13 16:32:12 +0100

    Mail in fase di sviluppo la console funz. in parte

--- Mail/myscipt.js
+++ Mail/myscipt.js
@@ -1,7 +1,14 @@
-// input utente attraverso un alert
+const aggMail = prompt("Inserire la propria mail"); // input utente attraverso l'utente
 
-//aggiunta array con mail preinserite
+const mailnelDatabase = [ "pippo@gmail.com", "pluto@gmail.com", "105@gmail.com" ]; //aggiunta array con mail preinserite
 
-//creare un ciclo per controllare se si ha gia una mail
+for(let i = 0; i < mailnelDatabase.length; i++){ //creare un ciclo per controllare se si ha gia una mail
+    
+    if( mailnelDatabase[i] == aggMail){
+        console.log("Mail già usata"); //Se la mail è stata già confermata stampare "mail già usata"
+    }
+    else if (mailnelDatabase[i] != aggMail){
+        console.log("Mail disponibile"); //confermare mail se non è stata già utilizzata
+    }
+} 
 
-//confermare mail se non è stata già utilizzata
