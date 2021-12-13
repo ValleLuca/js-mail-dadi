e5d537564185ee8863ce78e652a8333aa724be28
ValleLuca <valle.lucaAb@outlook.it>
2021-12-13 16:17:33 +0100

    Inizio JS mail

---
+++ Mail/myscipt.js
@@ -0,0 +1,7 @@
+// input utente attraverso un alert
+
+//aggiunta array con mail preinserite
+
+//creare un ciclo per controllare se si ha gia una mail
+
+//confermare mail se non è stata già utilizzata
