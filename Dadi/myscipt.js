5b6baa2c10dc56b259a9790375e51fad7d83e2b8
ValleLuca <valle.lucaAb@outlook.it>
2021-12-13 15:44:47 +0100

    Aggiunte ma senza risultati dadi

---
+++ Dadi/myscipt.js
@@ -0,0 +1,18 @@
+const vincitore = document.getElementById("win"); //Prendere id per stampare in html il vinctore
+
+//aggiungere variabili
+
+const utente = (math.floot(Math.random()*5+1)); //aggiungere generatore Utente
+const pc = []; //aggiungere generatire pc
+
+if(utete > pc){
+    console.log(utente);
+    vincitore.innerHTML = "utente";
+}else{
+    console.log(pc);
+    vincitore.innerHTML = "pc";
+}
+
+//stampare il vincitore in console
+
+//stampare il vincitore su schermo nell'html
